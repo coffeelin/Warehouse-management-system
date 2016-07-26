fd6660b1c30f06d1b1c722caab0fdde0171bf257
JsBlueCat <15618561709@163.com>
2016-07-26 16:01:23 +0800

    新增 模型ctx

--- WMS/WMSDAO/EntityContext.cs
+++ WMS/WMSDAO/EntityContext.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Configuration;
 
 namespace WMSDAO.Models
 {
     public class WMSContext : DbContext
     {
-      
-        
+        public WMSContext()
+			: base(ConfigurationManager.ConnectionStrings["WMSConnString"].ConnectionString) {
+            Database.SetInitializer<WMSContext>(null);
+        }
+
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Dine> Dines { get; set; }
+        public DbSet<DineDeatil> DineDetails { get; set; }
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+
     }
 }
