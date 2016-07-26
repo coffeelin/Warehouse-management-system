7b5b5f3cd510a7d3f863618293c5bfe40c3d97d9
JsBlueCat <15618561709@163.com>
2016-07-26 15:42:53 +0800

    添加 EF Nuget 包

---
+++ WMS/WMSDAO/EntityContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace WMSDAO.Models
+{
+    public class WMSContext : DbContext
+    {
+      
+        
+    }
+}
