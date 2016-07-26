96fc572b3261e501ead4f7a7ff1078afb5439590
JsBlueCat <15618561709@163.com>
2016-07-26 12:37:39 +0800

    添加了JSON 传输处理协议  和 函数处理返回 协议

---
+++ WMS/Protocol/JsonFunctionProtocols.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Protocol
+{
+
+    public class JsonError
+    {
+        public JsonError() { }
+        public JsonError(string errorMessage)
+        {
+            ErrorMessage = errorMessage;
+        }
+        public JsonError(string errorMessage, string errorPosition)
+        {
+            ErrorMessage = errorMessage;
+            ErrorPosition = errorPosition;
+        }
+        public bool Succeeded { get; set; } = false;
+        public string ErrorMessage { get; set; }
+        public string ErrorPosition { get; set; }
+    }
+
+
+    public class JsonSuccess
+    {
+        public JsonSuccess() : this("") { }
+        public JsonSuccess(string redirectUrl) : this(redirectUrl, null) { }
+        public JsonSuccess(object data) : this("", data) { }
+        public JsonSuccess(string redirectUrl, object data)
+        {
+            RedirectUrl = redirectUrl;
+            Data = data;
+        }
+        public bool Succeeded { get; set; } = true;
+        public string RedirectUrl { get; set; }
+        public object Data { get; set; }
+    }
+
+
+    public class FunctionResult
+    {
+        public FunctionResult(bool succeeded = true)
+        {
+            Succeeded = succeeded;
+        }
+
+        public FunctionResult(bool succeeded, string msg, string detail = null)
+        {
+            Succeeded = succeeded;
+            Message = msg;
+            Detail = detail;
+        }
+
+        public FunctionResult(bool succeeded, object data)
+        {
+            Succeeded = succeeded;
+            Data = data;
+        }
+
+        public FunctionResult(Exception e)
+        {
+            Succeeded = false;
+            Message = e.Message;
+        }
+
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+        public object Data { get; set; }
+    }
+
+
+}
