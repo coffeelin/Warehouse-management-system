6077755e21232b11692a78d309dda9e9696bd447
JsBlueCat <15618561709@163.com>
2016-07-26 16:11:43 +0800

    新增 WMSManager 类

---
+++ WMS/WMSDAO/WMSManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using WMSDAO.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+
+namespace WMSDAO
+{
+    public class WMSManager
+    {
+        public WMSManager()
+        {
+            ctx = new WMSContext();
+        }
+        protected WMSContext ctx;
+
+        public string ConnectionString
+        {
+            get
+            {
+                return ctx.Database.Connection.ConnectionString;
+            }
+        }
+    }
+}
