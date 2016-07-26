20419f78e0a8e509bf12c6266d2442af5b2d1fec
JsBlueCat <15618561709@163.com>
2016-07-26 13:34:21 +0800

    添加   引用程序集

---
+++ WMS/WMSDAOModels/ProductsModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WMSDAO.Models
+{
+
+    /// <summary>
+    /// 产品类
+    /// </summary>
+    class ProductsModels
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
